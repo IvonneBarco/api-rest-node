88479ef9def4a161cbda2751ecddda000e293f8b
IvonneBarco <ivonn1022@gmail.com>
2019-11-20 18:08:41 -0500

    Login y token

--- api-rest-node/controllers/userController.js
+++ api-rest-node/controllers/userController.js
@@ -4,6 +4,7 @@
 var validator = require('validator');
 var User = require('../models/userModel');
 var bcrypt = require('bcrypt-node');
+var jwt = require('.././services/jwt')
 
 var controller = {
 
@@ -110,7 +111,88 @@ var controller = {
                 password: validate_password
             });
         }
-    } //--- Close method save ---//
+    }, //--- Close method save ---//
+
+    login: function (request, response) {
+
+        // 1. Recoger parametros de la petición
+        var params = request.body;
+
+        // 2. Validar datos
+        var validate_email = !validator.isEmpty(params.email) && validator.isEmail(params.email);
+        var validate_password = !validator.isEmpty(params.password);
+
+        if (!validate_email || !validate_password) {
+            return response.status(500).send({
+                status: 'error',
+                code: 500,
+                message: 'Los datos son incorrectos'
+            });
+        }
+
+        // 3. Buscar usuarios que coincidan con el email
+        User.findOne({ email: params.email.toLowerCase() }, (error, user) => {
+            if (error) {
+                return response.status(500).send({
+                    message: 'Error al intentar identificarse'
+                });
+            }
+
+            if (!user) {
+                return response.status(500).send({
+                    message: 'El usuario no existe'
+                });
+            }
+
+            // 4. Si lo encuentra:
+
+            // - Comprobar la contraseña (coincidencia de email y password / bcrypt)
+            bcrypt.compare(params.password, user.password, (err, check) => {
+
+                // 5. Si es correcto:
+
+                if (check) {
+                    // - Generar token de jwt y devolverlo
+                    if (params.gettoken) {
+
+                        // 6. Devolver los datos
+                        return response.status(200).send({
+                            status: 'success',
+                            message: 'Se ha identificado correctamente',
+                            token: jwt.createToken(user)
+
+                        });
+                    } else {
+
+                        // - Limpiar objeto o ...
+                        user.password = null;
+
+                        // ... (Datos que necesita el front-end)
+                        // var user_data = {
+                        //     email: user.email,
+                        //     name: user.name
+                        // }
+
+                        // 6. Devolver los datos
+                        return response.status(200).send({
+                            status: 'success',
+                            message: 'Se ha identificado correctamente, pero aún no se genera el token',
+                            user
+
+                        });
+                    }
+
+                }
+
+                // 6. Devolver los datos
+                return response.status(500).send({
+                    status: 'Las credenciales no son correctas'
+                });
+            });
+
+        });
+
+    }
 };
 
 module.exports = controller;
