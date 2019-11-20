914051944d5055e0e901164ab072636acde05995
Ivonne Carolina Barco <ivonn1022@gmail.com>
2019-11-20 18:09:30 -0500

    Login y token (#5)

---
+++ api-rest-node/services/jwt.js
@@ -0,0 +1,21 @@
+'user strict'
+
+//Imports
+var jwt = require('jwt-simple'); //crea el token
+var moment = require('moment'); //valida el tiempo del token
+
+exports.createToken = function (user) {
+    
+    var payload = {
+        sub: user._id,
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+        role: user.role,
+        image: user.image,
+        iat: moment().unix(),
+        exp: moment().add(30, 'days').unix
+    };
+
+    return jwt.encode(payload, 'clave-secreta-para -generar-el-toke-@123');
+}
