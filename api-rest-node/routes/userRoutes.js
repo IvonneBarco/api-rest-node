11571d782b138d32f4017a7171f6bc66cc4b8959
IvonneBarco <ivonn1022@gmail.com>
2019-11-20 21:27:32 -0500

    Middleware y metodo actualizar

--- api-rest-node/routes/userRoutes.js
+++ api-rest-node/routes/userRoutes.js
@@ -4,14 +4,16 @@ var express = require('express');
 var UserController = require('../controllers/userController');
 
 var router = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
 //Rutas de prueba
 router.get('/probando', UserController.probando);
 router.post('/testeando', UserController.testeando);
 
 //Rutas de usuarios
-router.post('/create', UserController.save);
+router.post('/user', UserController.save);
 router.post('/login', UserController.login);
+router.put('/user', md_auth.authenticated, UserController.update);
 
 //export
 module.exports = router;
