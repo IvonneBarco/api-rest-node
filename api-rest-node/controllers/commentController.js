e89f1bcbcfcfca9048beae4a6e91b6abf2294379
IvonneBarco <ivonn1022@gmail.com>
2019-11-22 17:44:57 -0500

    Metodo de agregar comentario

--- api-rest-node/controllers/commentController.js
+++ api-rest-node/controllers/commentController.js
@@ -1,13 +1,91 @@
 'user stric'
 
+//Imports
+var Topic = require('../models/topicModel');
+var validator = require('validator');
+
 var controller = {
     add: function (request, response) {
-        return response.status(200).send({
-            status: 'success',
-            code: 200,
-            message: 'Metodo de añadir comentario'
-        });
-    },
+
+        // 1. Recoger el id del topic de la url
+        var topicId = request.params.topicId;
+
+        // 2. Find por id del topic
+        Topic.findById(topicId).exec((error, topic) => {
+
+            if (error) {
+                return response.status(500).send({
+                    status: 'error',
+                    code: 500,
+                    message: 'Error en la petición'
+                });
+            }
+
+            if (!topic) {
+                return response.status(404).send({
+                    status: 'error',
+                    code: 404,
+                    message: 'No existe el tema'
+                });
+            }
+
+            // 3. Comprobar objeto usuario y validar datos
+            if (request.body.content) {
+
+                //validar datos
+                try {
+                    var validate_content = !validator.isEmpty(request.body.content);
+
+                } catch (error) {
+                    return response.status(202).send({
+                        status: 'warning',
+                        code: 202,
+                        message: 'No has realizado ningún comentario'
+                    });
+                }
+
+                if (validate_content) {
+
+                    var comment = {
+                        user: request.user.sub,
+                        content: request.body.content
+                    };
+
+                    // 4. En la propiedad comments del objeto resultante hacer un push
+                    topic.comment.push(comment);
+
+                    // 5. Guardar el topic completo
+
+                    topic.save((error) => {
+                        if (error) {
+                            return response.status(500).send({
+                                status: 'warning',
+                                code: 500,
+                                message: 'Error al guardar el comentario'
+                            });
+                        }
+
+                        // 6. Devolver una respuesta
+                        return response.status(200).send({
+                            status: 'success',
+                            code: 200,
+                            message: 'El comentario se ha agregado',
+                            topic
+                        });
+                    });
+
+                } else {
+                    return response.status(202).send({
+                        status: 'warning',
+                        code: 202,
+                        message: 'No se ha validado los datos de comentario'
+                    });
+                }
+            } // --- Close obj content and user --- //
+
+        }); // --- Close add Topic findById --- //
+
+    }, // --- Close add method --- //
 
     update: function (request, response) {
         return response.status(200).send({
@@ -15,7 +93,7 @@ var controller = {
             code: 200,
             message: 'Metodo de editar comentario'
         });
-    },
+    }, // --- Close update method --- //
 
     delete: function (request, response) {
         return response.status(200).send({
@@ -23,7 +101,7 @@ var controller = {
             code: 200,
             message: 'Metodo de eliminar comentario'
         });
-    }
+    } // --- Close delete method --- //
 };
 
 module.exports = controller;
