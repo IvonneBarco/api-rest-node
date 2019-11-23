0315ca906f0608faae6bebd55b06195e56540e98
IvonneBarco <ivonn1022@gmail.com>
2019-11-22 20:07:57 -0500

    Configuración de cors en Node

--- api-rest-node/app.js
+++ api-rest-node/app.js
@@ -17,7 +17,16 @@ var comment_routes = require('./routes/commentRoutes');
 app.use(bodyParser.urlencoded({ extended: false })); //para que bodyParser funcione
 app.use(bodyParser.json()); //Para convertir la petición a un obj Json
 
-// . CORS
+// 7. CORS
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 // 6. Reescribir rutas
 app.use('/api', user_routes);
